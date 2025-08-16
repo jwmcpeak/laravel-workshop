335780d5d17269110e1b01fa991f2fe462098931
Jeremy McPeak <jwmcpeak@gmail.com>
2025-08-16 14:59:14 -0500

    Added the profile model

---
+++ app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Profile extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProfileFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        // 'name',
+        'display_name',
+        'handle',
+        'bio',
+        'avatar_url',
+    ];
+
+
+    public function user() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+}
