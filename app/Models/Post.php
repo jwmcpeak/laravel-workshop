38912bdaa198336aa93b6d7d8cad412f282d12c8
Jeremy McPeak <jwmcpeak@gmail.com>
2025-08-23 22:40:51 -0500

    implemented basic repost functionality

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -15,6 +15,7 @@ class Post extends Model
     protected $fillable = [
         'profile_id',
         'parent_id',
+        'repost_of_id',
         'content'
     ];
 
@@ -38,6 +39,10 @@ class Post extends Model
         return $this->hasMany(Post::class, 'repost_of_id');
     }
 
+    public function repostOf() : BelongsTo {
+        return $this->belongsTo(Post::class, 'repost_of_id');
+    }
+
     public static function publish(Profile $profile, string $content) : self {
         return static::create([
             'profile_id'=> $profile->id,
@@ -53,6 +58,15 @@ class Post extends Model
             'content' => $content,
             'parent_id' => $original->id,
             'repost_of_id' => null,
+        ]);
+    }
+
+    public static function repost(Profile $profile, Post $original, string $content = null) : self {
+        return static::create([
+            'profile_id'=> $profile->id,
+            'content' => $content,
+            'parent_id' => null,
+            'repost_of_id' => $original->id,
         ]);
     }
 }
